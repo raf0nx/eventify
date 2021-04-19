223c9d1d87ce991e52b1cd51ac5797069a1bcb38
raf0nx <69021954+raf0nx@users.noreply.github.com>
2021-04-20 00:03:00 +0200

    Laravel mix configuration, Vue scaffolding

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import Vuetify from 'vuetfy'
+
+Vue.use(Vuetify);
+
+import App from './components/App.vue'
+
+const app = new Vue({
+    el: '#app',
+    components: App,
+    vuetify: new Vuetify()
+});
